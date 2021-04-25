f8c3dd8215a42774195ea165a682540af614fbba
KenlerMrz <1284974404@qq.com>
2021-04-26 01:54:41 +0800

    Initial commit

---
+++ src/main/java/com/example/jdenmo/JdenmoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdenmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdenmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdenmoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdenmo/JdenmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdenmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdenmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
