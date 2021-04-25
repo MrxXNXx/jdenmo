da2e97b3b892411d4feb77425212a477a9523e36
KenlerMrz <1284974404@qq.com>
2021-04-26 02:04:58 +0800

    Update JdenmoApplication.java
    dddd

--- src/main/java/com/example/jdenmo/JdenmoApplication.java
+++ src/main/java/com/example/jdenmo/JdenmoApplication.java
@@ -2,7 +2,7 @@ package com.example.jdenmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//dddd
 @SpringBootApplication
 public class JdenmoApplication {
 
