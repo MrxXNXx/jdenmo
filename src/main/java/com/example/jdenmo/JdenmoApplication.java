3a18a3a6e129aa547ea47c40a79ee53df70df060
KenlerMrz <1284974404@qq.com>
2021-04-26 16:04:58 +0800

    Update JdenmoApplication.java
    djtest

--- src/main/java/com/example/jdenmo/JdenmoApplication.java
+++ src/main/java/com/example/jdenmo/JdenmoApplication.java
@@ -2,7 +2,7 @@ package com.example.jdenmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hello Mrdj
+//hello Mrdjtest
 @SpringBootApplication
 public class JdenmoApplication {
 
